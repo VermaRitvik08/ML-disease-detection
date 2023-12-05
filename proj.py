7a33a4a6e12cdbd7fa75e1164dff5d0be9926405
Khushjajoo <89994024+Khushjajoo@users.noreply.github.com>
2023-12-05 13:37:04 -0500

    Update proj.py

--- proj.py
+++ proj.py
@@ -1,52 +1,21 @@
-# %%
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python Docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load
-
 import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-
-# Input data files are available in the read-only "../input/" directory
-# For example, running this (by clicking run or pressing Shift+Enter) will list all files under the input directory
-
 import os
-# for dirname, _, filenames in os.walk('/kaggle/input'):
-#     for filename in filenames:
-#         print(os.path.join(dirname, filename))
-
-# You can write up to 20GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
-# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
-
-# %% [markdown]
-# **First we'll be importing all the necessary libraries**
-
-# %%
-#pip install tensorflow==2.5
-
-# %%
 import cv2
 import random
 from tqdm import tqdm
 from keras import preprocessing
 
-# %% [markdown]
-# **Reading the datasets from the directory**
-
-# %%
 data = pd.read_csv("full_df.csv")
 data.head(20)
 
-# %%
 def has_condn(term,text):
     if term in text:
         return 1
     else:
         return 0
-
-# %%
-
+        
 def process_dataset(data):
-    #create 2 more columns labelling them whether right or left cataract
     data["left_cataract"] = data["Left-Diagnostic Keywords"].apply(lambda x: has_condn("cataract",x))
     data["right_cataract"] = data["Right-Diagnostic Keywords"].apply(lambda x: has_condn("cataract",x))
   
@@ -68,35 +37,27 @@ def process_dataset(data):
     data["LO"] = data["Left-Diagnostic Keywords"].apply(lambda x: has_condn("drusen",x))
     data["RO"] = data["Right-Diagnostic Keywords"].apply(lambda x: has_condn("drusen",x))
     
-    #store the right/left cataract images ids in a array
     left_cataract_images = data.loc[(data.C ==1) & (data.left_cataract == 1)]["Left-Fundus"].values
     right_cataract_images = data.loc[(data.C == 1) & (data.right_cataract == 1)]["Right-Fundus"].values
   
-    #store the left/right normal image ids in a array
     left_normal = data.loc[(data.C == 0) & (data["Left-Diagnostic Keywords"] == "normal fundus")]['Left-Fundus'].sample(350,random_state=42).values
     right_normal = data.loc[(data.C == 0) & (data["Right-Diagnostic Keywords"] == "normal fundus")]['Right-Fundus'].sample(350,random_state=42).values
     
-    #store the left/right diabetes image ids
     left_diab = data.loc[(data.C == 0) & (data.LD == 1)]["Left-Fundus"].values
     right_diab = data.loc[(data.C == 0) & (data.RD == 1)]["Right-Fundus"].values 
 
-    #store the left/right glaucoma image ids
     left_glaucoma = data.loc[(data.C == 0) & (data.LG == 1)]["Left-Fundus"].values
     right_glaucoma = data.loc[(data.C == 0) & (data.RG == 1)]["Right-Fundus"].values 
-    
-    #store the left/right diabetes image ids
+
     left_hyper = data.loc[(data.C == 0) & (data.LH == 1)]["Left-Fundus"].values
     right_hyper = data.loc[(data.C == 0) & (data.RH == 1)]["Right-Fundus"].values 
     
-    #store the left/right diabetes image ids
     left_myopia = data.loc[(data.C == 0) & (data.LM == 1)]["Left-Fundus"].values
     right_myopia = data.loc[(data.C == 0) & (data.RM == 1)]["Right-Fundus"].values 
     
-    #store the left/right diabetes image ids
     left_age = data.loc[(data.C == 0) & (data.LA == 1)]["Left-Fundus"].values
     right_age = data.loc[(data.C == 0) & (data.RA == 1)]["Right-Fundus"].values 
     
-    #store the left/right diabetes image ids
     left_other = data.loc[(data.C == 0) & (data.LO == 1)]["Left-Fundus"].values
     right_other = data.loc[(data.C == 0) & (data.RO == 1)]["Right-Fundus"].values 
     
@@ -111,7 +72,6 @@ def process_dataset(data):
     
     return normalones,cataractones,diabones,glaucoma,hyper,myopia,age,other;
 
-# %%
 normal , cataract , diab, glaucoma , hyper , myopia , age, other = process_dataset(data);
 
 print("Dataset stats::")
@@ -125,8 +85,6 @@ print("Age Issues ::" , len(age))
 print("Other ::" , len(other))
 
 
-
-# %%
 dataset_dir = "preprocessed_images"
 image_size=224
 labels = []
@@ -134,8 +92,7 @@ dataset = []
 def dataset_generator(imagecategory , label):
     for img in tqdm(imagecategory):
         imgpath = os.path.join(dataset_dir,img);
-        
-        #now we try to read the image and resize it accordingly
+  
         try:
             image = cv2.imread(imgpath,cv2.IMREAD_COLOR)
             image = cv2.resize(image,(image_size,image_size))
@@ -145,19 +102,6 @@ def dataset_generator(imagecategory , label):
     random.shuffle(dataset);
     
     return dataset;
-
-# %% [markdown]
-# Generating Dataset
-
-# %%
-# Normal == 0
-# Cataract == 1
-# Diabetes == 2
-# Glaucoma == 3
-# HyperTension == 4
-# Myopia == 5
-# Age == 6
-# Other == 7
 
 dataset = dataset_generator(normal,0)
 dataset = dataset_generator(cataract,1)
@@ -192,33 +136,20 @@ for i in range(10):
     elif category == 6:
         label = "Age Issues"
     else:
-        label = "Other"
-  
-
-# %%
-#Divide the dataset into 2 predictors and target...
-#here predictors would be the image and target would be the label
+        label = "Other"  
 
 train_x = np.array([i[0] for i in dataset]).reshape(-1,image_size,image_size,3);
 train_y = np.array([i[1] for i in dataset])
 
 from sklearn.model_selection import train_test_split
 from tensorflow import keras
-#splitting the dataset
+
 x_train , x_test , y_train, y_test = train_test_split(train_x,train_y, test_size=0.2)
 
 y_train_cat = keras.utils.to_categorical(y_train,num_classes=8)
 
 y_test_cat = keras.utils.to_categorical(y_test,num_classes = 8)
 
-
-# %% [markdown]
-# 
-
-# %% [markdown]
-# Creating the Model
-
-# %%
 from keras.applications.vgg19 import VGG19
 import ssl
 import tensorflow as tf
@@ -242,19 +173,15 @@ model.add(keras.layers.Dense(8,activation="softmax"))
 
 model.summary()
 
-# %%
 model.compile(optimizer="adam",loss="categorical_crossentropy",metrics=["accuracy"])
-#u might have to apply train_generator and test_generator
-history = model.fit(x_train,y_train_cat,batch_size=32,epochs=1)
+
+history = model.fit(x_train,y_train_cat,batch_size=32,epochs=15)
 print(history)
 
-# %%
 loss,accuracy = model.evaluate(x_test,y_test_cat)
 print("Accuracy:",accuracy)
 
-# %%
 from sklearn.metrics import confusion_matrix,classification_report,accuracy_score
-# y_pred = np.array((model.predict(x_test) > 0.5).astype("int32"))
 
 y_pred = []
 for i in model.predict(x_test):
@@ -262,52 +189,8 @@ for i in model.predict(x_test):
 
 print(y_pred)
 
-# %%
 print(accuracy_score(y_test,y_pred))
 
-
-# for i in range(20):
-#     sample = random.choice(range(len(x_test)))
-#     image = x_test[sample]
-#     category = y_test[sample]
-#     pred_category = y_pred[sample]
-    
-#     if category== 0:
-#         label = "Normal"
-#     elif category == 1 :
-#         label = "Cataract"
-#     elif category == 2:
-#         label = "Diabetes"
-#     elif category == 3:
-#         label = "Glaucoma"
-#     elif category == 4:
-#         label = "Hypertension"
-#     elif category == 5:
-#         label = "Myopia"
-#     elif category == 6:
-#         label = "Age Issues"
-#     else:
-#         label = "Other"
-        
-#     if pred_category== 0:
-#         pred_label = "Normal"
-#     elif pred_category == 1 :
-#         pred_label = "Cataract"
-#     elif pred_category == 2:
-#         pred_label = "Diabetes"
-#     elif pred_category == 3:
-#         pred_label = "Glaucoma"
-#     elif pred_category == 4:
-#         pred_label = "Hypertension"
-#     elif pred_category == 5:
-#         pred_label = "Myopia"
-#     elif pred_category == 6:
-#         pred_label = "Age Issues"
-#     else:
-#         pred_label = "Other"
-        
-
-# %%
 
 def predicted_class(new_image_path):
     
@@ -353,27 +236,13 @@ def predicted_class(new_image_path):
     new_image = cv2.imread(new_image_path)
     new_image = cv2.resize(new_image, (224, 224)) 
     new_image = new_image / 255.0  
-
-    # Perform inference
+    
     predictions = model.predict(np.expand_dims(new_image, axis=0))
-
-    # Get the class with the highest probability
+    
     predicted_class = np.argmax(predictions)
     print(f"Actual: {label}")
     print(f"Predicted class: {pred_label}")
-    # Now 'predicted_class' contains the predicted class label for the new image
     return pred_label
-# new_image = cv2.imread(new_image_path)
-# new_image = cv2.resize(new_image, (224, 224)) 
-# new_image = new_image / 255.0  
-
-# # Perform inference
-# predictions = model.predict(np.expand_dims(new_image, axis=0))
-
-# # Get the class with the highest probability
-# predicted_class = np.argmax(predictions)
-
-# # Now 'predicted_class' contains the predicted class label for the new image
 
 
 
