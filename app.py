033d742e1979fed8b4a3e79b3ca732cbb6a3fe36
Khushjajoo <89994024+Khushjajoo@users.noreply.github.com>
2023-12-11 15:42:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from streamlit_option_menu import option_menu
 
 diabetes_model = pickle.load(open('diabetes_model.sav', 'rb'))
 
-heart_disease_model = pickle.load(open('heart_disease_model.sav','rb'))
+heart_disease_model = pickle.load(open('heart_model.sav','rb'))
 
 parkinsons_model = pickle.load(open('parkinsons_model.sav', 'rb'))
 
