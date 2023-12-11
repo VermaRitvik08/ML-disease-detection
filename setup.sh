c987378b9dbe783b1795fcb2e206de2f876b3d75
Khushjajoo <89994024+Khushjajoo@users.noreply.github.com>
2023-12-11 16:21:35 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,24 @@
 mkdir -p ~/.streamlit/
-echo "
+
+
+echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
-" > ~/.streamlit/config.toml
+" > ~/.streamlit/credentials.toml
+
 echo "
 [theme]
 primaryColor=’#020202’
 backgroundColor=’#c4c3c3’
 secondaryBackgroundColor=’#ebd316’
 font = ‘sans serif’
-[server]
-headless = true
-port = $PORT
-enableCORS = false
 " > ~/.streamlit/config.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
+
 
