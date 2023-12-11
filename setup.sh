1780821b0d247399e843b84d4a50eb0c6434651d
Khushjajoo <89994024+Khushjajoo@users.noreply.github.com>
2023-12-11 16:25:24 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,14 +6,6 @@ echo "\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
 
-echo "
-[theme]
-primaryColor=’#020202’
-backgroundColor=’#c4c3c3’
-secondaryBackgroundColor=’#ebd316’
-font = ‘sans serif’
-" > ~/.streamlit/config.toml
-
 echo "\
 [server]\n\
 headless = true\n\
